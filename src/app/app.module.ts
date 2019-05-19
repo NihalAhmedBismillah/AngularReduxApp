7c77ba66bbbed21fad59205fe94d1bd631f4a6ab
Nihal Ahmed <nihaltomail@gmail.com>
2019-05-19 22:23:30 +0530

    init: added todo list with redeux store

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgRedux, NgReduxModule } from '@angular-redux/store';
+import { IAppState, rootReducer, INITIAL_STATE } from './store';
+import { AppComponent } from './app.component';
+import { TodoOverviewComponent } from './todo-overview/todo-overview.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoOverviewComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgReduxModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+   constructor (private ngRedux:NgRedux<IAppState>){
+      this.ngRedux.configureStore(rootReducer,INITIAL_STATE)
+   }
+
+ }
